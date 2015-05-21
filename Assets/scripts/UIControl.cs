f57807f66c9709fb7ac95b3ef38294203d2c4fd1
Pirolf <yuchengtu.uwm@gmail.com>
2015-05-21 16:35:20 -0500

    a little ui

---
+++ Assets/scripts/UIControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour {
+	public Text remainingTimeText;
+	public void UpdateRemainingTime(){
+		if(remainingTimeText != null){
+			remainingTimeText.text = GameControl.self.timeUntilNext.ToString("0.0");
+		}
+		
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateRemainingTime();
+	}
+}
