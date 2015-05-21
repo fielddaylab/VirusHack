625626d23943fc1b200e8c86b5e6d4fd852b8ea3
Pirolf <yuchengtu.uwm@gmail.com>
2015-05-21 14:58:45 -0500

    change nucleic acid type

--- Assets/scripts/GameControl.cs
+++ Assets/scripts/GameControl.cs
@@ -3,18 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GameControl : MonoBehaviour {
+	public static GameControl self;
 	public Dictionary<string, AminoAcid> geneticCodeTable;
 	public AminoAcid aminoAcidPrefab;
+	public string sequence = "UUUAGCGCG";
+	public Material matA;
+	public Material matG;
+	public Material matU;
+	public Material matC;
+	public NucleicAcid activeNucleicAcid;
+
 	void Awake(){
 		geneticCodeTable = new Dictionary<string, AminoAcid>();
 		CreateAminoAcid("UUU", "UUC", null, null, "Phe");
 		CreateAminoAcid("UUA", "UUG", null, null, "Leu");
 		CreateAminoAcid("UCU", "UCC", "UCA", "UCG", "Ser");
 		CreateAminoAcid("UAU", "UAC", null, null, "Tyr");
-
-
-		
+		self = this;
 	}
+
 	public void CreateAminoAcid(string codon0, string codon1, string codon2, string codon3,
 		 string name){
 		AminoAcid aa = Instantiate(aminoAcidPrefab) as AminoAcid;
@@ -40,6 +47,18 @@ public class GameControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		char newType = activeNucleicAcid.name;
+		if(Input.GetKey("a")){
+			newType = 'A';
+		}else if(Input.GetKey("g")){
+			newType = 'G';
+		}else if(Input.GetKey("t")){
+			newType = 'T';
+		}else if(Input.GetKey("c")){
+			newType = 'C';
+		}
+		if(activeNucleicAcid != null && newType != activeNucleicAcid.name){
+			activeNucleicAcid.ChangeTypeTo(newType);
+		}
 	}
 }
