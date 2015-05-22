ba5348c26f4e25322d4b235f28759ca03e7c30e8
Kevin Alford <zeroeth@pixelflow.org>
2015-05-22 12:26:06 -0500

    Multiple animation sequences with delay and speed

---
+++ Assets/Sequence Segment/SequenceDelay.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SequenceDelay : MonoBehaviour {
+
+    public Animation anim;
+	public float delay = 1.0F;
+	public float speed = 0.1F;
+	public float start_time;
+	public bool started = false;
+
+    void Start()
+	{
+        anim = GetComponent<Animation>();
+        foreach (AnimationState state in anim)
+		{
+            state.speed = speed;
+        }
+
+		start_time = Time.time + delay;
+	}
+
+
+	void Update ()
+	{
+		if(!started && Time.time > start_time)
+		{
+			anim.Play();
+			started = true;
+		}
+	}
+}
